67ae0cce29a13e33e8086a254b5f2dd2229121fa
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-07-28 16:36:47 -0500

    Update WriteToPPm to include Vec3

--- RayTracer/RayTracer/WriteToPPM.cpp
+++ RayTracer/RayTracer/WriteToPPM.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include "Vec3.h"
 
 using namespace std;
 
@@ -12,12 +13,11 @@ int main() {
 
 	for (int i = ny - 1; i >= 0; i--) {
 		for (int j = 0; j < nx; j++) {
-			float r = float(j) / float(nx);
-			float g = float(i) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99*r);
-			int ig = int(255.99*g);
-			int ib = int(255.99*b);
+			Vec3 col(float(j) / float(nx), float(i) / float(ny), 0.2);
+			int ir = int(255.99*col.r());
+			int ig = int(255.99*col.g());
+			int ib = int(255.99*col.b());
+
 			output << ir << " " << ig << " " << ib << "\n";
 		}
 	}
