31b566bcfd94750ff62f148fa5e1c49b618f812a
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-07-29 18:17:12 -0500

    Create Ray class and update WriteToPPM for background colors

---
+++ RayTracer/RayTracer/Ray.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef RAYH
+#define RAYH
+#include "Vec3.h"
+
+class ray 
+{
+public:
+	ray() {}
+	ray(const Vec3& a, const Vec3& b) { A = a; B = b; }
+	Vec3 origin() const { return A; }
+	Vec3 direction() const { return B; }
+	Vec3 point_at_parameter(float t) const { return A + t * B; }
+
+	Vec3 A;
+	Vec3 B;
+};
+#endif // !RAYH
+
